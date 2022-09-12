e440d8cfbba81f09a2a90a6040404916223728c6
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-09-12 12:39:16 -0500

    Add final pages and start over button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,29 @@ var buttonStart = document.querySelector("#buttonStart");
 var pageStart = document.querySelector("#pageStart"); 
 var Q1 = document.querySelector("#Q1");
 var Q2 = document.querySelector("#Q2");
+// NOTE: getElementsByClassName doesn't require a . for the class
 var answerFalse = document.getElementsByClassName("false");
 var answerTrue = document.querySelector(".true");
 var resultText = document.querySelector("#resultText");
 var indivResults = document.querySelector("#indivResults"); 
 var initials = document.querySelector("#initials"); 
 var buttonInitials = document.querySelector("#buttonInitials"); 
+var pageFinal = document.querySelector("#pageFinal"); 
 var highScores = document.querySelector("#highScores"); 
+var buttonStartOver = document.querySelector("#buttonStartOver");
+var buttonReset = document.querySelector("#buttonReset")
+var test = document.querySelector("#test"); 
 
 
-var timer; 
-var score = 5;
-var initials = "";
-var highScore
+var score = 0;
+initials.textContext = initials; 
 
 // Function to hide all elements except the beginning statement
 function init() {
     Q1.style.display = "none"; 
     Q2.style.display = "none"; 
     indivResults.style.display = "none";
+    pageFinal.style.display = "none";
 }
 
 // Hides the opening statement and introduces the first question
@@ -50,12 +54,18 @@ answerTrue.addEventListener("click", function() {
 })
 
 
-// buttonInitials.addEventListener("click", function() {
-//     initials = "CBJ"; 
-//     indivResults.style.display = "none"; 
-//     // highScores.style.display = "block"; 
-//     test.textContent = initials + ": " + score
-// })
+buttonInitials.addEventListener("click", function() {
+    initials = initials.value; 
+    indivResults.style.display = "none"; 
+    pageFinal.style.display = "block";
+    test.textContent = initials + ": " + score
+})
+
+buttonStartOver.addEventListener("click", function() {
+    pageFinal.style.display = "none";
+    pageStart.style.display = "block"; 
+    score = 0; 
+})
 
 init();
 
